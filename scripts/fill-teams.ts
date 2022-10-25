c155ca628a3774b6d5810a8a8de99b46ca79aeaa
Logan Olson <loganolsonn@gmail.com>
2022-10-25 01:09:18 -0500

    Setup scraper script with Prisma, created default teamRouter

---
+++ scripts/fill-teams.ts
@@ -0,0 +1,40 @@
+import { prisma } from '../src/server/db/client';
+import puppeteer from 'puppeteer';
+
+console.log('hi0');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.espn.com/nba/teams', {
+    waitUntil: 'load',
+    timeout: 0,
+  });
+
+  const grabTeamInfo = async () => {
+    const rows = await page.$$('.TeamLinks');
+    let index = 94;
+    const formattedTeam = await Promise.all(
+      rows.map(async (row) => {
+        const name = await row.$eval('h2', (el) => el.innerHTML);
+        const logo = await row.$eval('img', (el) => el.src);
+        index = index + 1;
+        const id = index;
+        return {
+          name,
+          logo,
+          id,
+        };
+      })
+    );
+    return formattedTeam;
+  };
+  const fillTeams = async () => {
+    const teams = await grabTeamInfo();
+    await prisma.team.createMany({
+      data: teams,
+    });
+  };
+  await fillTeams();
+  await browser.close();
+})();
